f9208fbbecc895acef0c5a682db49c9397566458
Lammie <plkelly@yahoo.com>
2020-04-13 14:35:24 -0700

    Minor UI changes in saved.handlebars.  Adjusted delete article route in apiRoutes.js.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -85,7 +85,7 @@ module.exports = function (app) {
     // Route for grabbing a specific Article by id, populate it with it's note
     app.get("/articlesdel/:id", function (req, res) {
         // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
-        db.Article.deleteOne({ _id: req.params.aid })
+        db.Article.deleteOne({ _id: req.params.id })
             .then(function (dbArticle) {
                 res.json(dbArticle);
             })
@@ -117,9 +117,6 @@ module.exports = function (app) {
         // Create a new note and pass the req.body to the entry
         db.Note.create(req.body)
             .then(function (dbNote) {
-                // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
-                // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
-                // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
                 return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
             })
             .then(function (dbArticle) {
